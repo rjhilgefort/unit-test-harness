46ae96d36da14ced58dee6b775f6ea89aa592868
Rob Hilgefort <rjhilgefort@gmail.com>
2018-08-25 15:29:05 -0600

    Lamer way to expose Jest methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-// NOTE: put unit stuff in 'main' and create 'jest' folder
-//       which front loads the jest globals
+import expect from 'expect';
+import agnosticUnitTestCases from './unit-test-cases';
+import agnosticUnitTestHarness from './unit-test-harness';
 
-export { default as unitTestHarness } from './unit-test-harness';
-export { default as unitTestCases } from './unit-test-cases';
+// TODO: I really don't love these names. I think the proper
+//       solution would be to not have `expect` in this repo
+//       and instead create `unit-test-harness-jest` or something.
+//       ... I really don't want to maintain that though. I only need
+//       the Jest stuff.
+
+export { agnosticUnitTestHarness };
+export { agnosticUnitTestCases };
+
+export const unitTestHarness = agnosticUnitTestHarness(expect);
+export const unitTestCases = agnosticUnitTestCases(expect);
--- src/jest/index.ts
+++
@@ -1,6 +0,0 @@
-import expect from 'expect';
-import unitTestCasesMain from '../unit-test-cases';
-import unitTestHarnessMain from '../unit-test-harness';
-
-export const unitTestHarness = unitTestHarnessMain(expect);
-export const unitTestCases = unitTestCasesMain(expect);
